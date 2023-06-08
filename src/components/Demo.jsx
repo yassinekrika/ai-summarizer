6ca2ce5681940f4ab0eae99b85f8591031dd6f65
yassinekrika <yassinekrika2016@gmail.com>
2023-06-08 22:18:26 +0100

    redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick } from "../assets"
+import { useLazyGetSummaryQuery } from "../services/article"
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -7,8 +8,19 @@ const Demo = () => {
     summary: ""
   })
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
   const handleSubmit = async (e) => {
-    alert("SUbmitted")
+    e.preventDefault()
+    const { data } = await getSummary({ articleUrl: article.url})
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary }
+      setArticle(newArticle)
+
+      console.log(newArticle)
+    }
+
   }
 
   return (
